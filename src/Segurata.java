a62703202d59126ea9b19a8b2fa39e5f650b3c6a
Menta <mentadere@gmail.com>
2025-05-24 20:08:38 +0200

    Nueva funcionalidad

--- src/Segurata.java
+++ src/Segurata.java
@@ -7,11 +7,16 @@ public class Segurata {
         System.out.println("Dime tu nombre: ");
         String nombre = scan.nextLine();
 
+        System.out.println("Dime dónde quieres ir: ");
+        String discoteca = scan.nextLine();
+
         System.out.println("Dime tu edad: ");
         int edad = scan.nextInt();
 
-        if (edad >= 18) {
-            System.out.println("Puedes pasar.");
+        if (discoteca.equals("Cocoloco") && edad >= 18) {
+            System.out.println("Puedes pasar a Cocoloco.");
+        } else if (discoteca.equals("Coquitoloquito") && edad >= 13 && edad < 18) {
+            System.out.println("Puedes pasar a Coquitoloquito");
         } else {
             System.out.println("No puedes pasar.");
         }
